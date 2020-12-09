cfc5f8f68c93ffc458fa5fa2d3293afdefbee701
Lincoln Anders <lincolnanders5@gmail.com>
2020-12-09 14:41:52 -0500

    Fix path issues

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "NestedPublished",
+            path: "Sources",
             targets: ["NestedPublished"]
         ),
     ],

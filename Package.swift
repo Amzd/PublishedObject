45be6dc7cd2f3191be425aa5879cfe74c1e9bc52
Lincoln Anders <lincolnanders5@gmail.com>
2020-12-09 14:45:16 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,13 +13,12 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "NestedPublished",
-            path: "Sources",
             targets: ["NestedPublished"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "NestedPublished", dependencies: []),
+        .target(name: "NestedPublished", dependencies: [], path: "Sources"),
     ]
 )
